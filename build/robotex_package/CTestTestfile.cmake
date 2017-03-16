b4c0ef903397c7c16fda9034ec24fe6244647e9d
Raid Vellerind <raid.vellerind@gmail.com>
2017-03-16 07:39:12 +0200

    Removed folder from repository

--- build/robotex_package/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/raid/robotex_ws/src/robotex_package
-# Build directory: /home/raid/robotex_ws/build/robotex_package
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
